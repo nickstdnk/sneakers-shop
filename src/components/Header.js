7e5a54b9d9783e0a1e5c32f99fbb6cc1dc7e61dc
Nikolay Stadnik <nick.stdnk@gmail.com>
2021-07-01 14:09:19 +0700

    add component Card

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
       <div className="d-flex align-center">
         <img width={40} height={40} src="/img/logo.png" />
         <div>
-          <h3 className="text-uppercase">React Sneakers</h3>
+          <h3 className="text-uppercase">ZENDEN</h3>
           <p className="opacity-5">Магазин лучших кроссовок</p>
         </div>
       </div>
